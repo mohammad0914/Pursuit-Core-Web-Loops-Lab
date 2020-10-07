417c84e2d2b7f4f8d2f25404425a96ba6170c3c9
Corey Ladovsky <coreyskymusic@gmail.com>
2020-10-06 21:59:23 -0400

    loop questions extended

--- problems/loops.js
+++ problems/loops.js
@@ -143,3 +143,103 @@ function fizzBuzzPart1() {}
  */
 function fizzBuzzPart2() {}
 
+/**
+ * Takes in range (inclusive) and returns the sum of all numbers in that range.
+ 
+ * @param {number} min number 
+ * @param {number} max number 
+ * @returns {number} sum all all numbers from min to max
+ */
+function rangeSum() {}
+
+/**
+ * Takes in range (inclusive) and returns an array in decreasing order of all odd numbers
+ * in that range
+ 
+ * @param {number} min number 
+ * @param {number} max number 
+ * @returns {array} all odd numbers in range decreasing
+ */
+function rangeOdd() {}
+
+/**
+ * Takes in range (inclusive) and returns an array in increasing order of every other element. 
+
+ 
+ * @param {number} min number 
+ * @param {number} max number 
+ * @returns {array} includes every other element
+ */
+function rangeEveryOther() {}
+
+/**
+ * Takes in an array and a target. 
+ * Determines whether or not that target exists in the array
+ * Must be written with a while loop
+ * Must NOT contain built in includes method. 
+ * @param {array} elements 
+ * @param {number or string} target 
+ * @returns {boolean} true if the array contains our target, otherwise false
+ */
+function containsWhileLoop() {}
+
+/**
+ * Takes in an array and a target. 
+ * Determines whether or not that target exists in the array
+ * Must be written with a for loop
+ * Must NOT contain built in includes method. 
+ * @param {array} elements 
+ * @param {number or string} target 
+ * @returns {boolean} true if the array contains our target, otherwise false
+ */
+function containsForLoop() {}
+/**
+ * Takes in an array and a target. 
+ * Determines how many times the target exists in the array
+ * @param {array} elements 
+ * @param {number or string} target 
+ * @returns {number} number of occurances
+ */
+function targetCount() {}
+/**
+ * Takes in an array and a target. 
+ * Determines the first index that the target is found at. 
+ * If the target is not found it should return -1
+ * NOT allowed to use built in method indexOf 
+ * @param {array} elements 
+ * @param {number or string} target 
+ * @returns {number} first index found.
+ */
+function firstIndexFound() {}
+/**
+ * Takes in an array and a target. 
+ * Determines the last index that the target is found at. 
+ * If the target is not found it should return -1
+ * NOT allowed to use built in method lastIndexOf 
+ * @param {array} elements 
+ * @param {number or string} target 
+ * @returns {number} last index found.
+ */
+function firstIndexFound() {}
+
+/**
+ * Takes in an array. 
+ * Returns a new array where each element has been multiplied by it's index. 
+ * Exp: Input [2, 2, 2] Output: [0, 2, 4] because 0 * 2, 1 * 2, and 2 * 2
+ * @param {array} elements 
+ * @returns {array}
+ */
+function timesIndex() {}
+
+
+/**
+ * Takes in an array. 
+ * Returns a new array where each element is the cumulative sum so far. 
+ * Exp: Input [5, 2, 9] Output: [5, 7, 16] . 5 is the first element so added to 0.
+ *  7 because 5 + 2 is 7 
+ * 16 because 7 + 9 is 16
+ * @param {array} elements 
+ * @returns {array}
+ */
+function timesIndex() {}
+
